b2d88fc04a1a4bca884e6e48ab7f1ff59acbcb31
KamilBienias <kamil.bienias.math@gmail.com>
2021-01-21 15:48:21 +0100

    graph

---
+++ 02_dash_intro/05_graph.py
@@ -0,0 +1,14 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# nazwa __name__ to zmienna srodowiskowa
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout =
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
