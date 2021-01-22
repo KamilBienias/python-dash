93d90eb619ecb56d90711c0b7b07f741f172a353
KamilBienias <kamil.bienias.math@gmail.com>
2021-01-22 18:27:25 +0100

    textarea

---
+++ 03_dash_core/04_text_area.py
@@ -0,0 +1,28 @@
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# nazwa __name__ to zmienna srodowiskowa
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+
+    dcc.Textarea(
+        placeholder="Wprowadz wartosc",
+        style={"width": "100%"},
+        value=""  # przyjmuje to co wpisze do pola tekstowego
+    ),
+
+    dcc.Textarea(
+        placeholder="Wprowadz wartosc",
+        style={"width": "60%"}
+    ),
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
